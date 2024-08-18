ce1c7967611bbb53dd0f4946232f28c1141216ad
Geri Fixit <fixitgeri@gmail.com>
2024-08-18 16:10:34 +0400

    fixed auth req overload issue

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { Session } from "next-auth";
 import authConfig from "./auth.config";
 
 import {
@@ -7,11 +7,11 @@ import {
   publicRoutes,
   apiAuthPrefix,
 } from "@/routes";
-import { url } from "inspector";
+import { NextRequest } from "next/server";
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
+export default auth((req: NextRequest & { auth: Session | null }): Response | void => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
   const isApiAuthRoute = nextUrl.pathname.startsWith?.(apiAuthPrefix);
@@ -19,18 +19,18 @@ export default auth((req) => {
   const isAuthUrl = authRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
-    return null;
+    return;
   }
 
   if (isAuthUrl) {
     if (isLoggedIn)
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    return null;
+    return;
   }
   if (!isLoggedIn && !isPublicRoute)
     return Response.redirect(new URL("/auth/login", nextUrl));
 
-  return null;
+  return;
 });
 
 export const config = {
